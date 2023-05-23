b5b7265101025482663094db7db7295008d63647
Dominykas Vigricas <dominykas.vigricas@gmail.com>
2023-05-23 19:57:33 +0300

    fix router

--- server/src/api/auth/oauth/oauth.js
+++ server/src/api/auth/oauth/oauth.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const User = require('../../db/models/User');
-const jwt = require('jsonwebtoken');
 
 router.use('/discord', require('./providers/discord'));
 router.use('/github', require('./providers/github'));
