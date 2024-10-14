4f47befca286db2834c232397c26a22d878819bc
Dominykas Vigricas <dominykas.vigricas@gmail.com>
2024-10-14 22:31:01 +0300

    remove sendFile route

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const http = require('http');
-const cors = require('cors');
-const morgan = require('morgan');
-require('./db/db');
+const http = require("http");
+const cors = require("cors");
+const morgan = require("morgan");
+require("./db/db");
 const server = http.createServer(app);
-const { Server } = require('socket.io');
-const Message = require('./db/models/Message');
+const { Server } = require("socket.io");
+const Message = require("./db/models/Message");
 
-const chalk = require('chalk');
-const path = require('path');
+const chalk = require("chalk");
+const path = require("path");
 
 const io = new Server(server, {
   cors: {
     origin: true,
     credentials: true,
-    methods: ['GET', 'POST'],
+    methods: ["GET", "POST"],
   },
   allowEIO3: true,
 });
@@ -25,16 +25,16 @@ app.use(cors({ credentials: true, origin: true }));
 
 const morganMiddleware = morgan(function (tokens, req, res) {
   return [
-    '\n',
-    chalk.hex('#34ace0').bold(tokens.method(req, res)),
-    chalk.hex('#ffb142').bold(tokens.status(req, res)),
-    chalk.hex('#ff5252').bold(tokens.url(req, res)),
-    chalk.hex('#2ed573').bold(tokens['response-time'](req, res) + ' ms'),
-    chalk.hex('#f78fb3').bold('@ ' + tokens.date(req, res)),
-    chalk.yellow(tokens['remote-addr'](req, res)),
-    chalk.hex('#fffa65').bold('from ' + tokens.referrer(req, res)),
-    chalk.hex('#1e90ff')(tokens['user-agent'](req, res)),
-  ].join(' ');
+    "\n",
+    chalk.hex("#34ace0").bold(tokens.method(req, res)),
+    chalk.hex("#ffb142").bold(tokens.status(req, res)),
+    chalk.hex("#ff5252").bold(tokens.url(req, res)),
+    chalk.hex("#2ed573").bold(tokens["response-time"](req, res) + " ms"),
+    chalk.hex("#f78fb3").bold("@ " + tokens.date(req, res)),
+    chalk.yellow(tokens["remote-addr"](req, res)),
+    chalk.hex("#fffa65").bold("from " + tokens.referrer(req, res)),
+    chalk.hex("#1e90ff")(tokens["user-agent"](req, res)),
+  ].join(" ");
 });
 
 app.use(morganMiddleware);
@@ -49,45 +49,45 @@ app.use(express.json());
 //   credentials: true,
 // };
 // app.options('*', cors(corsOptions));
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use('/api', require('./api/api'));
+// app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use("/api", require("./api/api"));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
-io.on('connection', async (socket) => {
-  socket.on('newMessage', async (payload) => {
+io.on("connection", async (socket) => {
+  socket.on("newMessage", async (payload) => {
     const message = await Message.findOne({
       _id: payload.message._id,
-    }).populate('author');
+    }).populate("author");
     if (payload.channel._id == message.channel) {
       io.emit(`newMessage`, message);
     }
   });
 
-  socket.on('editMessage', async (payload) => {
+  socket.on("editMessage", async (payload) => {
     const message = await Message.findOne({
       _id: payload,
-    }).populate('author');
+    }).populate("author");
 
     io.emit(`editMessage`, message);
   });
 
-  socket.on('deleteMessage', async (payload) => {
+  socket.on("deleteMessage", async (payload) => {
     io.emit(`messageDeleted`, payload);
   });
 
-  socket.on('updateGuild', async (payload) => {
-    io.emit('updateGuild', payload);
+  socket.on("updateGuild", async (payload) => {
+    io.emit("updateGuild", payload);
   });
 
-  socket.on('updateGuildIcon', async () => {
-    io.emit('updateGuildIcon');
+  socket.on("updateGuildIcon", async () => {
+    io.emit("updateGuildIcon");
   });
 
-  socket.on('userStatusChanged', () => {
-    io.emit('userStatusChanged');
+  socket.on("userStatusChanged", () => {
+    io.emit("userStatusChanged");
   });
 });
 
